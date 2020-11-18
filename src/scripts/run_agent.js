f1a3ec8b0b371c2bd245b890882f4305f3a24e0b
Akrobate <fedorov.artiom@gmail.com>
2020-11-18 13:31:23 +0100

    Cleaned agent code

--- src/scripts/run_agent.js
+++ src/scripts/run_agent.js
@@ -1,41 +1,23 @@
 'use strict'
 
-// Récupération du client mongodb
-var bodyParser = require('body-parser');
-var CryptoCompare = require('./libs/CryptoCompare');
-var MinerGate = require('./libs/MinerGate');
-var orm = require('./libs/orm');
-
 let Agent = require('./business_modules/Agent');
 let Market = require('./business_modules/Market');
 
 let market = new Market();
 
-
 market.setStartDate("2014-09-01");
 market.setEndDate("2017-05-21");
 
-market.loadData( function(data) {
-
-    let agent = new Agent(10.0);
+market.loadData((data) => {
+    const agent = new Agent(10.0);
     agent.setMarket(market);
 
     let current_price = market.getCurrentPrice();
     agent.setCurrentPrice(current_price);
-    //console.log(agent);
 
-    let currency = 'BTC';
-    let paidWithCurrency = 'EUR';
-    let payment = 5.0;
-
-console.log(market.getPricesCount());
     for (let i = 0; i < market.getPricesCount() - 1; i++) {
-        // market.getPricesCount() - 1
         current_price = market.getNextPrice();
-        //console.log(current_price);
         agent.setCurrentPrice(current_price);
         agent.trade();
     }
-
-
 });
