813e9058a8419054949d9801a68ef1cc2bc109a0
Akrobate <fedorov.artiom@gmail.com>
2021-03-21 20:29:54 +0100

    Updated Market file structure eslint

--- src/business_modules/Market.js
+++ src/business_modules/Market.js
@@ -1,67 +1,82 @@
-'use strict'
+'use strict';
 
-var orm = require('../libs/orm');
-
-/**
- *
- */
+const orm = require('../libs/orm');
 
 class Market {
 
+    // eslint-disable-next-line require-jsdoc
     constructor() {
-        this.start_date = "";
-        this.end_date = "";
+        this.start_date = null;
+        this.end_date = null;
         this.data = [];
         this.current = 0;
     }
 
-
+    /**
+     * @returns {Number}
+     */
     getPricesCount() {
         return this.data.length;
     }
 
+    /**
+     * @returns {Number}
+     */
     getNextPrice() {
         this.current++;
         return this.getCurrentPrice();
     }
 
-
+    /**
+     * @returns {Number}
+     */
     getCurrentPrice() {
         return this.data[this.current];
     }
 
+    /**
+     * @param {String} currency
+     * @returns {Number}
+     */
     getCurrentEvolution(currency) {
-        return (this.data[this.current][currency] - this.data[this.current - 1][currency]) / this.data[this.current][currency] * 100;
+        return (this.data[this.current][currency] - this.data[this.current - 1][currency])
+            / this.data[this.current][currency] * 100;
     }
 
-
+    /**
+     * @returns {Number}
+     */
     getCurrentPointer() {
         return this.current;
     }
 
+    // eslint-disable-next-line require-jsdoc
     setStartDate(date) {
         this.start_date = date;
     }
 
-
+    // eslint-disable-next-line require-jsdoc
     setEndDate(date) {
         this.end_date = date;
     }
 
+    // eslint-disable-next-line require-jsdoc
     loadData(callback) {
 
         const collection_name = 'historical_prices';
 
-        let options = {
-            "limit": 10000,
-            "sort":  { date: 1 }
+        const options = {
+            limit: 10000,
+            sort: {
+                date: 1,
+            },
         };
 
-        let find = {
+        const find = {
             date: {
+                $gte: new Date(this.start_date),
                 $lte: new Date(this.end_date),
-                $gte: new Date(this.start_date)
-            }
+            },
         };
 
         orm.find(collection_name, find, options, (data) => {
