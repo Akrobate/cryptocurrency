ca54393062abd368fdc9b85af6fb7b5a59770cf2
Akrobate <fedorov.artiom@gmail.com>
2021-03-29 16:06:59 +0200

    Created inclusion helper for api repo

---
+++ src/repositories/api/index.js
@@ -0,0 +1,15 @@
+const {
+    MinerGate,
+} = require('./MinerGate');
+const {
+    CryptoCompare,
+} = require('./CryptoCompare');
+const {
+    Binance,
+} = require('./Binance');
+
+module.exports = {
+    CryptoCompare,
+    MinerGate,
+    Binance,
+};
