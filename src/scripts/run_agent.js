9b851089ec67804d92950fd3c07fd039bbcb5ddd
Akrobate <fedorov.artiom@gmail.com>
2021-04-02 14:44:08 +0200

    Fixed agent eslint

--- src/scripts/run_agent.js
+++ src/scripts/run_agent.js
@@ -1,21 +1,21 @@
-'use strict'
+'use strict';
 
-let Agent = require('./business_modules/Agent');
-let Market = require('./business_modules/Market');
+const Agent = require('./business_modules/Agent');
+const Market = require('./business_modules/Market');
 
-let market = new Market();
+const market = new Market();
 
-market.setStartDate("2014-09-01");
-market.setEndDate("2017-05-21");
+market.setStartDate('2014-09-01');
+market.setEndDate('2017-05-21');
 
-market.loadData((data) => {
+market.loadData(() => {
     const agent = new Agent(10.0);
     agent.setMarket(market);
 
     let current_price = market.getCurrentPrice();
     agent.setCurrentPrice(current_price);
 
-    for (let i = 0; i < market.getPricesCount() - 1; i++) {
+    for (let iterator = 0; iterator < market.getPricesCount() - iterator; iterator++) {
         current_price = market.getNextPrice();
         agent.setCurrentPrice(current_price);
         agent.trade();
