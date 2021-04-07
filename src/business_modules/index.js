b2b5084363ec9c75b8eaa48f0cbb2bf4094cb50b
Akrobate <fedorov.artiom@gmail.com>
2021-04-07 13:27:26 +0200

    Added inclusion file

---
+++ src/business_modules/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const {
+    Agent,
+} = require('./Agent');
+
+const {
+    Market,
+} = require('./Market');
+
+const {
+    OperationsHistory,
+} = require('./OperationsHistory');
+
+module.exports = {
+    Agent,
+    Market,
+    OperationsHistory,
+};
