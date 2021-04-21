3c85b04e7202ed59117331985ae4018c7dfcf20a
Akrobate <fedorov.artiom@gmail.com>
2021-04-21 13:56:30 +0200

    Updated run agent with trader agent

--- src/scripts/run_agent.js
+++ src/scripts/run_agent.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const Agent = require('./business_modules/Agent');
+const {
+    TraderAgent,
+} = require('./business_modules');
 const Market = require('./business_modules/Market');
 
 const market = new Market();
@@ -9,15 +11,15 @@ market.setStartDate('2014-09-01');
 market.setEndDate('2017-05-21');
 
 market.loadData(() => {
-    const agent = new Agent(10.0);
-    agent.setMarket(market);
+    const trader_agent = new TraderAgent(10.0);
+    trader_agent.setMarket(market);
 
     let current_price = market.getCurrentPrice();
-    agent.setCurrentPrice(current_price);
+    trader_agent.setCurrentPrice(current_price);
 
     for (let iterator = 0; iterator < market.getPricesCount() - iterator; iterator++) {
         current_price = market.getNextPrice();
-        agent.setCurrentPrice(current_price);
-        agent.trade();
+        trader_agent.setCurrentPrice(current_price);
+        trader_agent.trade();
     }
 });
