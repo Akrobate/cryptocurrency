23f6bee4de6847705cbeb671fc41e9e2c2a31da7
Akrobate <fedorov.artiom@gmail.com>
2021-04-24 13:52:19 +0200

    LoadCsvFile

--- src/business_modules/OperationsHistory.js
+++ src/business_modules/OperationsHistory.js
@@ -1,10 +1,19 @@
 'use strict';
 
+const {
+    CsvFile,
+} = require('../repositories');
+
+const {
+    Operation,
+} = require('./Operation');
+
 class OperationsHistory {
 
 
     // eslint-disable-next-line require-jsdoc
     constructor() {
+        this.csv_file = CsvFile.getInstance();
         this.data = [];
         this.current_pointer = -1;
     }
@@ -30,6 +39,27 @@ class OperationsHistory {
             return true;
         }
         return false;
+    }
+
+    /**
+     *
+     * @param {*} file
+     * @param {*} status_code_filter
+     * @return {Primse}
+     */
+    loadCsvOperations(file) {
+        return this.csv_file
+            .readLinePerLineCsvFile(
+                file,
+                (data) => {
+                    console.log(data);
+                    // this.add(data);
+
+                    this.add(new Operation(data));
+
+
+                }
+            );
     }
 }
 
