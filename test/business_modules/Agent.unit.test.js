f211f5db13f52ea6a212652be5f8a823a88bbf94
Akrobate <fedorov.artiom@gmail.com>
2021-04-26 20:56:33 +0200

    Added simple agent test

---
+++ test/business_modules/Agent.unit.test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const {
+    expect,
+} = require('chai');
+
+const {
+    Agent,
+} = require('../../src/business_modules');
+
+describe.only('Agent unit test', () => {
+
+    it('Should be able to init Agent object', () => {
+        const agent = new Agent('Smith');
+        expect(agent).to.have.property('name', 'Smith');
+    });
+
+    it.skip('Should be able to generate wallets from history file');
+});
