8ae0843b4f393a13743317d2cf0773e1ea53e9b1
Akrobate <fedorov.artiom@gmail.com>
2021-05-03 13:10:26 +0200

    Adapted script for debugging

--- src/scripts/real_time_agent_situation.js
+++ src/scripts/real_time_agent_situation.js
@@ -2,17 +2,22 @@
 
 const {
     Agent,
-} = require('../src/business_modules');
+} = require('../business_modules');
 
+const csv_file = './data/binance_history.csv';
 
 (async () => {
 
-    const agent = Agent.buildAgent();
-    await agent.loadOperationFile(seed_csv_file);
-    agent.generateWalletsState();
-    const agent_balance = await agent.calculateAgentBalance('EUR');
-    const eur_wallet = await agent.getBalanceFromWallet('EUR');
-    console.log(agent_balance);
-    console.log(eur_wallet);
+    try {
+        const agent = Agent.buildAgent();
+        await agent.loadOperationFile(csv_file);
+        agent.generateWalletsState();
+        const agent_balance = await agent.calculateAgentBalance('EUR');
+        const eur_wallet = await agent.getBalanceFromWallet('EUR');
+        console.log(agent_balance);
+        console.log(eur_wallet);
+    } catch (error) {
+        console.log(error.response.data);
+    }
 
 })();
