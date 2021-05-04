148af6575d88b124b76d1e5efdc596f45513bac9
Akrobate <fedorov.artiom@gmail.com>
2021-05-04 13:07:54 +0200

    Added update prices euro in wallet

--- src/business_modules/Wallet.js
+++ src/business_modules/Wallet.js
@@ -1,11 +1,17 @@
 'use strict';
 
+const {
+    Binance,
+} = require('../repositories/api/Binance');
+
 class Wallet {
 
     // eslint-disable-next-line require-jsdoc
     constructor(currency) {
         this.currency = currency;
         this.balance = null;
+        this.balance_euro = 0;
+        this.price_euro = null;
         this.history = [];
     }
 
@@ -19,11 +25,21 @@ class Wallet {
      */
     static buildWallet(currency, balance = 0, history = null) {
         const wallet = new Wallet(currency);
+        wallet.injectDependencies(Binance.getInstance());
         wallet.setBalance(balance);
         if (history) {
             wallet.setHistory(history);
         }
         return wallet;
+    }
+
+
+    /**
+     * @param {Binance} binance_repository
+     * @returns {void}
+     */
+    injectDependencies(binance_repository) {
+        this.binance_repository = binance_repository;
     }
 
     /**
@@ -34,12 +50,30 @@ class Wallet {
         this.balance = balance;
     }
 
+
+    /**
+     * @returns {String}
+     */
+    getCurrency() {
+        return this.currency;
+    }
+
+
     /**
      * @returns {Number}
      */
     getBalance() {
         return this.balance;
     }
+
+
+    /**
+     * @returns {Number}
+     */
+    getBalanceEuro() {
+        return this.balance_euro;
+    }
+
 
     /**
      * @param {Array} history
@@ -77,6 +111,20 @@ class Wallet {
             date: new Date(),
             amount,
         });
+    }
+
+
+    /**
+     * @async
+     * @returns {void}
+     */
+    async updateBalanceEuro() {
+        if (this.getBalance() !== 0 && this.currency !== 'EUR') {
+            const result = await this.binance_repository
+                .adaptSymbolAndGetLastestPrice(`${this.currency}EUR`);
+            this.price_euro = Number(result.price);
+            this.balance_euro = this.price_euro * this.getBalance();
+        }
     }
 
 }
