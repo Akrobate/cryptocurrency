c338143021efeb328554144c6843f74bbbb9dcd3
Akrobate <fedorov.artiom@gmail.com>
2021-05-13 14:19:07 +0200

    Preview situation script improved

--- src/scripts/real_time_agent_situation.js
+++ src/scripts/real_time_agent_situation.js
@@ -6,9 +6,25 @@ const {
 
 const csv_file = './data/binance_history.csv';
 
-function formatWalletDisplay(wallet) {
-    const str = `${wallet.getCurrency()} \t ${wallet.getBalance()} \t ${wallet.getBalanceEuro()} \t ${wallet.price_euro}`;
+function formatWalletDisplay(wallet, agent) {
+
+    const str = `${wallet.getCurrency()} ============
+    Ballance:       ${wallet.getBalance()} ${wallet.getCurrency()}
+    Euro ballance:  ${wallet.getBalanceEuro()} euro
+    Euro price:     ${wallet.price_euro} euro
+    USDT price:     ${wallet.price_usdt} USDT`;
+
     console.log(str);
+
+
+    agent
+        .getOpenOrders()
+        .filter((item) => item.symbol.includes(wallet.getCurrency()))
+        .forEach((order) => {
+            console.log('\x1b[32m%s\x1b[0m', `    --- Position ${order.symbol}`);
+            console.log('\x1b[32m%s\x1b[0m', `        ${order.side} price ${order.price}`);
+        });
+
 }
 
 function format4Decimals(num) {
@@ -21,10 +37,13 @@ function format4Decimals(num) {
     try {
         const agent = Agent.buildAgent();
         await agent.loadOperationFile(csv_file);
+        agent.loadOpenOrders();
         agent.generateWalletsState();
         const agent_balance = await agent.calculateAgentBalance('EUR');
         const eur_wallet = await agent.getBalanceFromWallet('EUR');
-        agent.getWalletsWithAmount().map(formatWalletDisplay);
+        agent.getWalletsWithAmount().map(
+            (item) => formatWalletDisplay(item, agent)
+        );
         const revenue = eur_wallet + agent_balance;
         console.log('Revenue: ', format4Decimals(revenue));
 
