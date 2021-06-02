7aa31672de2f5731c5684147c7acf41de20bf76b
Akrobate <fedorov.artiom@gmail.com>
2021-06-02 12:58:04 +0200

    Cleaned binance history prices import clean

--- src/scripts/binance_history_prices.js
+++ src/scripts/binance_history_prices.js
@@ -3,75 +3,26 @@
 const moment = require('moment');
 
 const {
-    Binance,
-} = require('../repositories/api');
-const {
-    JsonFile,
-} = require('../repositories/JsonFile');
-
-const {
     BinancePriceDownloader,
 } = require('../business_modules/BinancePriceDownloader');
 
-const binance_price_dowloader = BinancePriceDownloader.getInstance();
-const json_file = JsonFile.getInstance();
+const params = {
+    symbol: 'ADAUSDT',
+    interval_value: 1,
+    interval_unit: 'm',
+};
 
-// const binance = Binance.getInstance();
+
+const binance_price_dowloader = BinancePriceDownloader.getInstance();
 
 function timeToDate(time) {
     return moment.unix(Number(time) / 1000).format('MM/DD/YYYY hh:mm:ss');
 }
 
+// eslint-disable-next-line no-unused-vars
 function displayRow(item) {
     return timeToDate(item[0]);
 }
-
-
-async function updateHistory(symbol, interval_value, interval_unit) {
-
-    const filename = `${symbol}_${interval_value}${interval_unit}.json`;
-    console.log(filename);
-    const millis_interval = binance_price_dowloader
-        .getSecondMillisDuration(interval_value, interval_unit);
-
-    json_file.setFileName(filename);
-
-
-    let start_time = 0;
-    const end_time = moment().format('x') - millis_interval;
-
-    let saved_data = [];
-
-    try {
-        saved_data = await json_file.getData();
-        for (let i = saved_data.length - 10; i < saved_data.length; i++) {
-            console.log(timeToDate(saved_data[i][0]));
-            console.log(saved_data[i]);
-        }
-        console.log(saved_data.length);
-        const last = saved_data[saved_data.length - 1];
-        console.log(timeToDate(last[0]));
-        start_time = Number(last[0]) + millis_interval;
-    } catch (error) {
-        start_time = await binance_price_dowloader.findStartTime(symbol);
-    }
-
-    const result = await binance_price_dowloader.getData(
-        symbol,
-        interval_value,
-        interval_unit,
-        start_time,
-        end_time,
-        1000
-    );
-
-    await json_file.saveData([].concat(saved_data, result));
-
-    console.log(result.map(displayRow));
-
-
-}
-
 
 binance_price_dowloader.setBucketCallback((data) => {
     console.log(`${data.current_bucket} / ${data.total_buckets} - ${timeToDate(data.current_params.endTime)}`);
@@ -79,51 +30,15 @@ binance_price_dowloader.setBucketCallback((data) => {
 
 (async () => {
 
-    // await updateHistory('ADAUSDT', 1, 'm');
+    await binance_price_dowloader.updateHistory(
+        params.symbol,
+        params.interval_value,
+        params.interval_unit
+    );
 
-    const resp = await binance_price_dowloader.downloadedFileIntegrityCheck('DOTUSDT', 1, 'm');
-    console.log(resp);
-    return ;
-
+    // const resp = await binance_price_dowloader.downloadedFileIntegrityCheck('DOTUSDT', 1, 'm');
+    // console.log(resp);
     // const start_date = await binance_price_dowloader.findStartTime('DOTUSDT');
     // const aaa = await binance_price_dowloader.findStartTime('XLMUSDT');
     // console.log(timeToDate(start_date));
-
-
-    const result = await binance_price_dowloader.getData(
-        // 'XLMUSDT',
-        'ADAUSDT',
-        // 'DOTUSDT',
-        1,
-        'm',
-        // moment()
-        //     .subtract(29, 'days')
-        //     .format('x'),
-        // start_date,
-        1539928740000,
-        // moment()
-        //     .subtract(22, 'days')
-        //     .format('x'),
-        1539941400000,
-        1000
-    );
-    // console.log(result);
-    // console.log(result.map(displayRow));
-    result.forEach(item => console.log(item[0] + " " + displayRow(item) )  );
-
-    // const params_1 = {
-    //    symbol: 'XLMUSDT',
-    //    interval: '1w',
-    //    endTime: moment()
-    //        .subtract(7, 'days')
-    //        .format('x'),
-    //    startTime: moment()
-    //        .subtract(14, 'days')
-    //        .format('x'),
-    //    limit: 1,
-    // };
-
-    // const result_1 = await binance.getCandlestickData(params_1);
-    // console.log(result_1.map(displayRow));
-
 })();
